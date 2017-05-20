99174b81a84cb644eff33d7c6cf1234baf8dbfeb
Aknik <kinka@tutanota.de>
2017-05-20 18:10:25 +0200

    Add files via upload

---
+++ pruebas.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1100.00)
+
+Adafruit_BME280 bme; // I2C
+
+void setup() {
+
+Wire.begin(4,5);
+Serial.begin(115200);
+Serial.println(F("BME280 test"));
+
+if (!bme.begin(0x76)) {
+Serial.println("Could not find a valid BME280 sensor, check wiring!");
+while (1);
+}
+}
+
+void loop() {
+Serial.print("Temperatura: ");
+Serial.print(bme.readTemperature());
+Serial.println(" *C");
+
+Serial.print("Presión: ");
+
+Serial.print(bme.readPressure() / 100.0F);
+Serial.println(" hPa");
+
+//Serial.print("Approx. Altitude = ");
+//Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+//Serial.println(" m");
+
+Serial.print("Humedad: ");
+Serial.print(bme.readHumidity());
+Serial.println(" %");
+
+Serial.println();
+delay(10000);
+}
+
---
+++ webtemp.ino
@@ -0,0 +1,252 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <TimeLib.h>
+
+
+#define SEALEVELPRESSURE_HPA (1100.00)
+
+// WiFi parameters
+const char* ssid = "WLAN_0BB0";
+const char* password = "999999999";
+WiFiServer server(80);
+
+Adafruit_BME280 bme; // I2C
+
+
+
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+ *  Lookup the IP address for the host name instead */
+ unsigned int localPort = 2390;      // local port to listen for UDP packets
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "hora.roa.es";
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+
+
+void setup() {
+
+// Connect to WiFi network
+WiFi.mode(WIFI_STA);
+Serial.println();
+Serial.println();
+Serial.print("Connecting to ");
+Serial.println(ssid);
+
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected");
+
+// Start the server
+server.begin();
+Serial.println("Server started");
+
+// Print the IP address
+Serial.println(WiFi.localIP());
+
+Serial.println("Starting UDP");
+udp.begin(localPort);
+Serial.print("Local port: ");
+Serial.println(udp.localPort());
+
+//get a random server from the pool
+  WiFi.hostByName(ntpServerName, timeServerIP); 
+
+  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+  // wait to see if a reply is available
+  delay(1000);
+  
+  int cb = udp.parsePacket();
+  if (!cb) {
+    Serial.println("no packet yet");
+  }
+  else {
+    // Serial.print("packet received, length=");
+    // Serial.println(cb);
+    // We've received a packet, read the data from it
+    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+    //the timestamp starts at byte 40 of the received packet and is four bytes,
+    // or two words, long. First, esxtract the two words:
+
+    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+    // combine the four bytes (two words) into a long integer
+    // this is NTP time (seconds since Jan 1 1900):
+    unsigned long secsSince1900 = highWord << 16 | lowWord;
+    //Serial.print("Seconds since Jan 1 1900 = " );
+    //Serial.println(secsSince1900);
+
+    // now convert NTP time into everyday time:
+    // Serial.print("Unix time = ");
+    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+    const unsigned long seventyYears = 2208988800UL;
+    // subtract seventy years:
+    unsigned long epoch = secsSince1900 - seventyYears;
+    // print Unix time:
+    //Serial.println(epoch);
+    // print the hour, minute and second:
+    Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+    Serial.print((epoch  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+    Serial.print(':');
+    if ( ((epoch % 3600) / 60) < 10 ) {
+      // In the first 10 minutes of each hour, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.print((epoch  % 3600) / 60); // print the minute (3600 equals secs per minute)
+    Serial.print(':');
+    if ( (epoch % 60) < 10 ) {
+      // In the first 10 seconds of each minute, we'll want a leading '0'
+      Serial.print('0');
+    }
+    Serial.println(epoch % 60); // print the second
+
+
+    setTime(epoch);
+    adjustTime(3600*2);
+    
+  }
+
+
+Wire.begin(4,5);
+Serial.begin(115200);
+Serial.println(F("BME280 test"));
+
+if (!bme.begin(0x76)) {
+Serial.println("Could not find a valid BME280 sensor, check wiring!");
+while (1);
+                      }
+}
+
+void loop() {
+Serial.println();
+Serial.print("Temperatura: ");
+Serial.print(bme.readTemperature());
+float temp = bme.readTemperature();
+Serial.println(" *C");
+
+Serial.print("Presión: ");
+Serial.print(bme.readPressure() / 100.0F);
+float pat = (bme.readPressure()/ 100.0F);
+Serial.println(" hPa");
+
+//Serial.print("Approx. Altitude = ");
+//Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+//Serial.println(" m");
+
+Serial.print("Humedad: ");
+Serial.print(bme.readHumidity());
+float humid = bme.readHumidity();
+Serial.println(" %");
+
+//Serial.println();
+displayCurrentDate();
+displayCurrentTime();
+
+
+float dPt = dewPointFast(temp, humid);
+
+WiFiClient client = server.available();
+client.println("HTTP/1.1 200 OK");
+client.println("Content-Type: text/html");
+client.println("Connection: close");  // the connection will be closed after completion of the response
+client.println("Refresh: 10");  // refresh the page automatically every 5 sec
+client.println();
+client.println("<!DOCTYPE html>");
+client.println("<html xmlns='http://www.w3.org/1999/xhtml'>");
+client.println("<head>\n<meta charset='UTF-8'>");
+client.println("<title>ESP8266 & BME280 Sensor</title>");
+client.println("<link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' rel='stylesheet'>");
+client.println("</head>\n<body>");
+client.println("<div class='container'>");
+client.println("<div class='panel panel-default' margin:15px>");
+client.println("<div class='panel-heading'>");
+client.println("<H2>ESP8266 & BME280 Sensor</H2>");
+client.println("<div class='panel-body' style='background-color: powderblue'>");
+client.println("<pre>");
+client.print("Temperatura °C  : ");
+client.println(temp, 2);
+client.print("Humedad %       : ");
+client.println(humid, 2);
+client.print("P.Atmos.  hPa   : ");
+client.print(pat, 2);
+client.print("                          ");
+//client.print("Temp.rocio  (°C): ");
+// client.println(dPt,2 );
+client.println("</pre>");
+client.println("</div>");
+client.println("</div>");
+client.println("</div>");
+client.print("</body>\n</html>");
+
+
+delay(10000);
+}
+
+
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  //Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
+
+
+// reference: http://en.wikipedia.org/wiki/Dew_point
+double dewPointFast(double celsius, double humidity)
+{
+  double a = 17.271;
+  double b = 237.7;
+  double t = (a * celsius) / (b + celsius) + log(humidity*0.01);
+  double Td = (b * t) / (a - t);
+  return Td;
+}
+
+void displayCurrentTime() {
+  Serial.print(hour());
+  Serial.print(":");
+  Serial.print(minute());
+  Serial.print(":");
+  Serial.print(second());
+  Serial.println();
+  //delay(1000);
+}
+void displayCurrentDate() {
+  Serial.print(day());
+  Serial.print("-");
+  Serial.print(month());
+  Serial.print("-");
+  Serial.print(year());
+  Serial.print(" ");
+  //delay(1000);
+}
