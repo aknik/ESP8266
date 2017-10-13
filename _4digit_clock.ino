d5e9734a2b53b33d5bd1b4a3ee1545a6f5a5d61b
Aknik <kinka@tutanota.de>
2017-10-13 12:48:45 +0200

    Update _4digit_clock.ino

--- _4digit_clock.ino
+++ _4digit_clock.ino
@@ -2,6 +2,7 @@
 #include <NTPClient.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <Time.h>
  
 const int CLK = D6; //Set the CLK pin connection to the display
 const int DIO = D5; //Set the DIO pin connection to the display
@@ -35,7 +36,7 @@ int numCounter = 0;
 int minutos = 0;
 int hora = 0;
 int TimeZone = 2;
-const char *ssid     = "WLAN_XXXX";
+const char *ssid     = "WLAN_XX";
 const char *password = ".999999999.";
 
 TM1637Display display(CLK, DIO); //set up the 4-Digit Display.
@@ -46,17 +47,14 @@ NTPClient timeClient(ntpUDP, "hora.roa.es", TimeZone * 3600, 600000);
 void setup(){
   
   Serial.begin(115200);
-  Serial.println("");
-  WiFi.disconnect();
-  delay(500);        //necessary ???
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-
-  while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
-    Serial.print ( "." );
+  WiFi.mode(WIFI_STA); 
+  WiFi.begin(ssid, password); 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+     delay(100);
   }
+  Serial.print("\n\nConexión Wifi.\nIP address: ");
+  Serial.println(WiFi.localIP());
   
   display.setBrightness(0x0a); //set the diplay to maximum brightness 0x0a
   timeClient.begin();
@@ -66,10 +64,20 @@ void setup(){
 void loop() {
   
 timeClient.update();
+
+time_t t = timeClient.getEpochTime();
+setTime(t);
+
 Serial.println(timeClient.getFormattedTime());
+LCDprint();
+
+}
+
+
+void LCDprint() {
   
-hora = timeClient.getHours();
-minutos = timeClient.getMinutes();
+hora = hour();
+minutos = minute();
 
 data[0] = digit_pattern[hora / 10];
 data[1] = digit_pattern[hora % 10 ];
